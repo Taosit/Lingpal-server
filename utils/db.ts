02baf048f095404a00efde1e15cffdd0286c6a8c
Jingxuan <taosit099@gmail.com>
2023-08-08 17:38:28 -0700

    Fixed env

--- utils/db.ts
+++ utils/db.ts
@@ -2,17 +2,18 @@ import { MongoClient } from "https://deno.land/x/mongo@v0.31.2/mod.ts";
 import { config } from "https://deno.land/x/dotenv/mod.ts";
 const { DATABASE_URI, ENVIRONMENT } = config();
 
+const client = new MongoClient();
+
 export const connectToDB = async () => {
-  const client = new MongoClient();
   const databaseUri =
     ENVIRONMENT === "development" ? DATABASE_URI : Deno.env.get("DATABASE_URI");
   if (!databaseUri) {
     throw new Error("Missing env variable: DATABASE_UTI");
   }
-  await client.connect(DATABASE_URI);
+  await client.connect(databaseUri);
   return client.database("lingpal");
 };
 
-// export const disconnectDB = async () => {
-//   client.close();
-// };
+export const disconnectDB = async () => {
+  client.close();
+};
